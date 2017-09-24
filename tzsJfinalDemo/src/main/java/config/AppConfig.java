554fc27d414ea506c5702b16349f1f96bb6948ad
unknown <1569890780@qq.com>
2017-09-24 16:25:47 +0800

    JfinalDemo1

---
+++ tzsJfinalDemo/src/main/java/config/AppConfig.java
@@ -0,0 +1,29 @@
+package config;
+import com.jfinal.config.*;
+import com.jfinal.core.JFinal;
+
+public class AppConfig extends JFinalConfig {
+
+    public void configConstant(Constants me) {
+        me.setDevMode(true);
+    }
+
+    public void configRoute(Routes me) {
+        me.add("/",AppController.class);
+    }
+
+    public void configPlugin(Plugins plugins) {
+
+    }
+
+    public void configInterceptor(Interceptors interceptors) {
+
+    }
+
+    public void configHandler(Handlers handlers) {
+
+    }
+    public static void main(String[] arg){
+        JFinal.start("src/main/webapp",80,"/",5);
+    }
+}
