9cb1b9e61332b5a9a094182a0a30968097e3a1fe
unknown <1569890780@qq.com>
2018-04-27 21:05:55 +0800

    fix

--- tzsJfinalDemo/src/main/java/config/AppConfig.java
+++ tzsJfinalDemo/src/main/java/config/AppConfig.java
@@ -1,21 +1,50 @@
 package config;
 import com.jfinal.config.*;
 import com.jfinal.core.JFinal;
+import com.jfinal.plugin.activerecord.ActiveRecordPlugin;
+import com.jfinal.plugin.activerecord.Model;
+import com.jfinal.plugin.druid.DruidPlugin;
+import com.jfinal.template.Engine;
+import controller.UserController;
+import model.User;
 
 public class AppConfig extends JFinalConfig {
 
+    /**
+     * 常量配置
+     * */
     public void configConstant(Constants me) {
         me.setDevMode(true);
     }
 
+    /**
+     * 路由配置
+     * */
     public void configRoute(Routes me) {
         me.add("/",AppController.class);
+        me.add("/user", UserController.class);
     }
 
+    public void configEngine(Engine me) {
+
+    }
+
+
+    /**
+     * 插件配置
+     * */
     public void configPlugin(Plugins plugins) {
-
+        loadPropertyFile("JDBC.properties");
+        DruidPlugin dp = new DruidPlugin(getProperty("url"), getProperty("username"), getProperty("password"));
+        plugins.add(dp);
+        ActiveRecordPlugin arp = new ActiveRecordPlugin(dp);
+        plugins.add(arp);
+        arp.addMapping("user", User.class);
     }
 
+    /**
+     * 拦截器配置
+     * */
     public void configInterceptor(Interceptors interceptors) {
 
     }
@@ -25,6 +54,7 @@ public class AppConfig extends JFinalConfig {
     }
     public static void main(String[] arg){
 
-        JFinal.start("src/main/webapp",80,"/",5);
+         JFinal.start("src/main/webapp",8081,"/");
     }
+
 }
--- tzsJfinalDemo/src/main/java/config/AppController.java
+++ tzsJfinalDemo/src/main/java/config/AppController.java
@@ -1,5 +1,6 @@
 package config;
 import com.jfinal.core.Controller;
+import com.jfinal.plugin.druid.DruidPlugin;
 
 public class AppController extends Controller {
     public void index(){
